60b37197cd29aa9f799d1f17ee73c5f943728e6e
rahma <rahma.putrianasaricoc@gmail.com>
2018-04-01 13:26:14 +0700

    two services run

--- app/src/main/java/com/rp/rahmawatiputrianasari/jobschedulerproject/MainActivity.java
+++ app/src/main/java/com/rp/rahmawatiputrianasari/jobschedulerproject/MainActivity.java
@@ -1,13 +1,62 @@
 package com.rp.rahmawatiputrianasari.jobschedulerproject;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.firebase.jobdispatcher.Constraint;
+import com.firebase.jobdispatcher.FirebaseJobDispatcher;
+import com.firebase.jobdispatcher.GooglePlayDriver;
+import com.firebase.jobdispatcher.Job;
+import com.firebase.jobdispatcher.Lifetime;
+import com.firebase.jobdispatcher.RetryStrategy;
+import com.firebase.jobdispatcher.Trigger;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String JOB_TAG = "JOB_TAG";
+    private static final String JOB_TAG2 = "JOB_TAG2";
+    FirebaseJobDispatcher firebaseJobDispatcher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        firebaseJobDispatcher = new FirebaseJobDispatcher(new GooglePlayDriver(this));
+    }
+
+    public void startJob(View view) {
+        Job job = firebaseJobDispatcher.newJobBuilder()
+                .setService(Service.class)
+                .setLifetime(Lifetime.FOREVER)
+                .setRecurring(true)
+                .setTag(JOB_TAG)
+                .setTrigger(Trigger.executionWindow(10, 10))
+                .setRetryStrategy(RetryStrategy.DEFAULT_EXPONENTIAL)
+                .setReplaceCurrent(false)
+                .setConstraints(Constraint.ON_ANY_NETWORK)
+                .build();
+        firebaseJobDispatcher.mustSchedule(job);
+
+        Job job2 = firebaseJobDispatcher.newJobBuilder()
+                .setService(Service2.class)
+                .setLifetime(Lifetime.FOREVER)
+                .setRecurring(true)
+                .setTag(JOB_TAG2)
+                .setTrigger(Trigger.executionWindow(15, 15))
+                .setRetryStrategy(RetryStrategy.DEFAULT_EXPONENTIAL)
+                .setReplaceCurrent(false)
+                .setConstraints(Constraint.ON_ANY_NETWORK)
+                .build();
+        firebaseJobDispatcher.mustSchedule(job2);
+        Toast.makeText(getApplicationContext(), "job scheduled", Toast.LENGTH_LONG).show();
+    }
+
+    public void endJob(View view) {
+        firebaseJobDispatcher.cancel(JOB_TAG);
+        firebaseJobDispatcher.cancel(JOB_TAG2);
+        Toast.makeText(getApplicationContext(), "job canceled", Toast.LENGTH_LONG).show();
+
     }
 }
---
+++ app/src/main/java/com/rp/rahmawatiputrianasari/jobschedulerproject/Service.java
@@ -0,0 +1,45 @@
+package com.rp.rahmawatiputrianasari.jobschedulerproject;
+
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Created by rahmawatiputrianasari on 4/1/18.
+ */
+
+
+
+public class Service extends com.firebase.jobdispatcher.JobService {
+
+    BackgroundTask backgroundTask;
+    @Override
+    public boolean onStartJob(final com.firebase.jobdispatcher.JobParameters job) {
+        Toast.makeText(getApplicationContext(), "START!!!", Toast.LENGTH_LONG).show();
+        Log.e("lalala","START");
+        backgroundTask = new BackgroundTask(){
+            @Override
+            protected void onPostExecute(String s) {
+                Log.e("lalala","asyntask");
+                Toast.makeText(getApplicationContext(),"message from background"+s,Toast.LENGTH_LONG).show();
+                jobFinished(job,false);
+            }
+        };
+        backgroundTask.execute();
+        return true;
+    }
+
+    @Override
+    public boolean onStopJob(com.firebase.jobdispatcher.JobParameters job) {
+        return true;
+    }
+
+    public static class BackgroundTask extends AsyncTask<Void,Void,String>{
+
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            return "from asyntask";
+        }
+    }
+}
